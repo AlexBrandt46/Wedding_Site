830a8dd0719685a85a76e26442d9a6e5f0eadc07
Alex Brandt <ajb3155@gmail.com>
2026-03-15 15:58:58 -0500

    Fix clock

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -1,24 +1,27 @@
-import { between, type Duration } from "duration-fns";
+import { between, normalize, type Duration } from 'duration-fns';
 
-const weddingDate = new Date(2026, 8, 27, 14);
+const weddingDate = new Date(2026, 8, 12, 14);
 
 export function getTimeDifference(): Duration {
   const today = new Date();
-  console.log(today);
-  const diff = between(weddingDate, today);
+  const diff = normalize(between(weddingDate, today));
+  const SEPT_DAYS = 30;
+  const dayDiff = SEPT_DAYS - diff.days;
 
-  console.log(diff)
-
-  // JS Dates use 0-based index of months, days, hours, minutes, and seconds
+  const dayPast =
+    diff.days > 0 ||
+    (diff.days === 0 && diff.hours > 0) ||
+    (diff.days === 0 && diff.hours === 0 && diff.minutes > 0) ||
+    (diff.days === 0 && diff.hours === 0 && diff.minutes === 0 && diff.minutes > 0);
 
   return {
     years: 0,
-    months: 11 + diff.months, // Month difference is negative for some dumb reason
-    weeks: diff.weeks,
-    days: diff.days,
+    months: 11 + diff.months + (dayPast ? 1 : 0), // Month difference is relative to calendar and not actual date for some dumb reason
+    weeks: Math.floor(dayDiff / 7),
+    days: dayDiff % 7,
     hours: 23 - diff.hours,
     minutes: 59 - diff.minutes,
     seconds: 59 - diff.seconds,
-    milliseconds: 0
-  }
+    milliseconds: 0,
+  };
 }
