c92e9218525f447d5a10ad74454c6be51926684e
AlexBrandt46 <ajb3155@gmail.com>
2026-05-09 11:18:23 -0500

    Remove images page, move image gallery to story (need to make smaller)

--- src/components/Photos/PhotoGallery.tsx
+++
@@ -1,5 +0,0 @@
-import Paper from '@mui/material/Paper';
-
-export default function PhotoGallery() {
-  return <Paper className="pagePaper"></Paper>;
-}
--- src/types/SupabaseTypes.ts
+++ src/types/SupabaseTypes.ts
@@ -3,5 +3,13 @@ export type StoryEntry = {
   header: string;
   his_text: string;
   hers_text: string;
+  images: string[] | undefined;
   [key: string]: unknown;
 };
+
+export interface StorageImage {
+  name: string;
+  path: string;
+  src: string;
+  alt: string;
+}
