228b18e9dc7b61baca210b19029a9a84a38501a3
AlexBrandt46 <ajb3155@gmail.com>
2026-05-11 21:49:23 -0500

    Update Accomodations

--- src/components/EventInfo/EventInfo.tsx
+++ src/components/EventInfo/EventInfo.tsx
@@ -39,67 +39,96 @@ export default function EventInfo() {
         </Typography>
       </Box>
       <br />
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.357936109817!2d-86.2034736228249!3d43.07596968957139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88198152002a1cb3%3A0x9fc82a6170eb75d2!2sThe%20Lilley%20Mansion%20Bed%20and%20Breakfast!5e0!3m2!1sen!2sus!4v1768167177460!5m2!1sen!2sus"
-        width="100%"
-        height="450"
-        style={{ border: 0, borderRadius: '4px' }}
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Grand Haven Map"
-      ></iframe>
+      <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.357936109817!2d-86.2034736228249!3d43.07596968957139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88198152002a1cb3%3A0x9fc82a6170eb75d2!2sThe%20Lilley%20Mansion%20Bed%20and%20Breakfast!5e0!3m2!1sen!2sus!4v1768167177460!5m2!1sen!2sus"
+          width="100%"
+          height="450"
+          style={{ border: 0, borderRadius: '4px' }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Grand Haven Map"
+        />
+      </Box>
       <br />
       <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         <Typography variant="h6">Dress Code</Typography>
         <Typography variant="body1">Garden Party/Bridgerton (be fun/colorful/creative)</Typography>
       </Box>
-      <br />
-      <b>Schedule of Events:</b>
-      <List>
-        <ListItem>
-          <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-            <Typography variant="body1">1 PM:</Typography>
-            <Typography variant="body1">Pre-Ceremony Greeting Time</Typography>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography variant="body1">2 PM:</Typography>
-            <Typography variant="body1">Ceremony</Typography>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography variant="body1">3 PM:</Typography>
-            <Typography variant="body1">Cocktail Hour</Typography>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography variant="body1">4 PM:</Typography>
-            <Typography variant="body1">Reception</Typography>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography variant="body1">9 PM:</Typography>
-            <Typography variant="body1">End of Reception</Typography>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography variant="body1">9:30 PM:</Typography>
-            <Typography variant="body1">Bar-Hopping in Spring Lake and Grand Haven</Typography>
-          </Box>
-        </ListItem>
-      </List>
+      <br />{' '}
+      <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        <Typography variant="h6">Schedule of Events</Typography>
+        <List>
+          <ListItem>
+            <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+              <Typography variant="body1">1 PM:</Typography>
+              <Typography variant="body1">Pre-Ceremony Greeting Time</Typography>
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography variant="body1">2 PM:</Typography>
+              <Typography variant="body1">Ceremony</Typography>
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography variant="body1">3 PM:</Typography>
+              <Typography variant="body1">Cocktail Hour</Typography>
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography variant="body1">4 PM:</Typography>
+              <Typography variant="body1">Reception</Typography>
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography variant="body1">9 PM:</Typography>
+              <Typography variant="body1">End of Reception</Typography>
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography variant="body1">9:30 PM:</Typography>
+              <Typography variant="body1">Bar-Hopping in Spring Lake and Grand Haven</Typography>
+            </Box>
+          </ListItem>
+        </List>
+      </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         <Typography variant="h6">Overnight Accommodations</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" gutterBottom>
           For those wishing to stay nearby, there are several hotels and numerous AirBnbs, bed and
-          breakfasts, and VRBOs within a short distance of the venue.
+          breakfasts, and VRBOs within a short distance of the venue. A few have been included
+          below!
         </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'row', gap: '1rem' }}>
+          <Box>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5828.752856111001!2d-86.21604776013719!3d43.07558086502502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x881981abb73111b1%3A0x75dce87e973abba4!2sHoliday%20Inn%20Grand%20Haven%20-%20Muskegon%20by%20IHG!5e0!3m2!1sen!2sus!4v1778553832599!5m2!1sen!2sus"
+              width="600"
+              height="450"
+              style={{ border: 0, borderRadius: '4px' }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
+          <Box>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5369.527957250377!2d-86.23367462866109!3d43.062040559753136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sHotels!5e0!3m2!1sen!2sus!4v1778554090993!5m2!1sen!2sus"
+              width="600"
+              height="450"
+              style={{ border: 0, borderRadius: '4px' }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
+        </Box>
       </Box>
       <br />
       <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
