323dd16c3b34c74d6b4c9861793aacd6c97114d1
AlexBrandt46 <ajb3155@gmail.com>
2026-06-07 19:29:06 -0500

    try fixing font on mobile

--- eslint.config.js
+++ eslint.config.js
@@ -7,19 +7,27 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import react from 'eslint-plugin-react';
 
 export default defineConfig([
-  globalIgnores(['dist', 'node_modules', 'coverage', 'build', '.vscode', 'github']),
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-      react.configs.recommended,
-    ],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: globals.browser,
-    },
-  },
+	globalIgnores([
+		'dist',
+		'node_modules',
+		'coverage',
+		'build',
+		'.vscode',
+		'.github',
+		'.agents',
+	]),
+	{
+		files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs['recommended-latest'],
+			reactRefresh.configs.vite,
+			react.configs.recommended,
+		],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			globals: globals.browser,
+		},
+	},
 ]);
--- src/App.tsx
+++ src/App.tsx
@@ -7,79 +7,83 @@ import MobileTabList from './components/Menu/MobileMenuDrawer';
 import WideTabList from './components/Menu/WideTabList';
 
 function App() {
-  const minHeaderMatch = useMediaQuery('(min-width:516px)');
+	const minHeaderMatch = useMediaQuery('(min-width:516px)');
 
-  const theme = createTheme({
-    typography: {
-      fontFamily: 'Butler',
-    },
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
-          @font-face {
-            font-family: 'Butler';
-            font-style: normal;
-            font-display: swap;
-            font-weight: 700;
-            src: local("Butler-Bold"), url("./assets/fonts/serif/Butler-Bold.woff2") format("woff2");
-          }
+	const theme = createTheme({
+		typography: {
+			fontFamily: 'Butler',
+		},
+		components: {
+			MuiCssBaseline: {
+				styleOverrides: `
+@font-face {
+	font-family: 'Butler';
+	font-style: normal;
+	font-display: swap;
+	font-weight: 700;
+	src:
+		local('Butler_Medium'),
+		url('./assets/fonts/serif-keep/Butler.woff2') format('woff2');
+}
         `,
-      },
-      MuiAlert: {
-        styleOverrides: {
-          root: {
-            variants: [
-              {
-                props: { severity: 'error' },
-                style: {
-                  color: 'var(--red)',
-                  backgroundColor: 'var(--salmon)',
-                },
-              },
-              {
-                props: { severity: 'info' },
-                style: {
-                  backgroundColor: 'var(--gold)',
-                },
-              },
-            ],
-          },
-        },
-      },
-    },
-  });
+			},
+			MuiAlert: {
+				styleOverrides: {
+					root: {
+						variants: [
+							{
+								props: { severity: 'error' },
+								style: {
+									color: 'var(--red)',
+									backgroundColor: 'var(--salmon)',
+								},
+							},
+							{
+								props: { severity: 'info' },
+								style: {
+									backgroundColor: 'var(--gold)',
+								},
+							},
+						],
+					},
+				},
+			},
+		},
+	});
 
-  const [tab, setTab] = React.useState('1');
-  const [uid, setUid] = React.useState('');
+	const [tab, setTab] = React.useState('1');
+	const [uid, setUid] = React.useState('');
 
-  React.useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const tabParam = params.get('tab');
-    const uidParam = params.get('uid');
+	React.useEffect(() => {
+		const params = new URLSearchParams(window.location.search);
+		const tabParam = params.get('tab');
+		const uidParam = params.get('uid');
 
-    if (tabParam) {
-      setTab(tabParam);
-    }
+		if (tabParam) {
+			setTab(tabParam);
+		}
 
-    if (uidParam) {
-      setUid(uidParam);
-    }
-  }, []);
+		if (uidParam) {
+			setUid(uidParam);
+		}
+	}, []);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          top: 0,
-          justifyContent: 'center',
-          fontFamily: 'Butler !important',
-        }}
-      >
-        {minHeaderMatch && <WideTabList tab={tab} uid={uid} setTab={setTab} />}
-        {!minHeaderMatch && <MobileTabList tab={tab} uid={uid} setTab={setTab} />}
-      </Box>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Box
+				sx={{
+					top: 0,
+					justifyContent: 'center',
+					fontFamily: 'Butler !important',
+				}}
+			>
+				{minHeaderMatch && <WideTabList tab={tab} uid={uid} setTab={setTab} />}
+				{!minHeaderMatch && (
+					<MobileTabList tab={tab} uid={uid} setTab={setTab} />
+				)}
+			</Box>
+		</ThemeProvider>
+	);
 }
 
 export default App;
