3a6aab1bfd739d0cc425c0bcdd3f421bc6aba14b
AlexBrandt46 <ajb3155@gmail.com>
2026-06-29 22:06:31 -0500

    refactor: move divider style to const variable

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,13 +1,15 @@
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 
+const dividerStyle = { margin: '1rem 0', width: '100%' };
+
 export default function PageHeader({ title }: { title: string }) {
 	return (
 		<>
 			<Typography variant="h5" gutterBottom>
 				{title}
 			</Typography>
-			<Divider sx={{ margin: '1rem 0', width: '100%' }} />
+			<Divider sx={dividerStyle} />
 		</>
 	);
 }
