06241ff97b778f2248a9cae7894d4337051d1595
AlexBrandt46 <ajb3155@gmail.com>
2026-06-29 23:35:18 -0500

    refactor: add style to main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,13 +4,15 @@ import styles from './Main.module.css';
 
 import proposalBanner from '../../assets/images/proposal_banner.jpg';
 
+const mainStyle = { display: 'flex', flexDirection: 'column' };
+
 export default function Main() {
-  return (
-    <Paper className={styles.pagePaper} sx={{ display: 'flex', flexDirection: 'column' }}>
-      <CountDownTimer />
-      <div style={{ display: 'block' }}>
-        <img className={styles.usImage} style={{ marginTop: '1rem', borderRadius: '4px' }} src={proposalBanner} />
-      </div>
-    </Paper>
-  );
+	return (
+		<Paper className={styles.pagePaper} sx={mainStyle}>
+			<CountDownTimer />
+			<div style={{ display: 'block' }}>
+				<img className={styles.usImage} style={{ marginTop: '1rem', borderRadius: '4px' }} src={proposalBanner} />
+			</div>
+		</Paper>
+	);
 }
